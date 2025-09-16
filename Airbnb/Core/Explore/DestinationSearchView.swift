45642b74013604c83851436a5bfe4c4c2505edec
kirtidhwaj <kirtdhwajpatra@gmail.com>
2025-09-16 21:02:43 +0530

    Added destination Search view

---
+++ Airbnb/Core/Explore/DestinationSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationSearchView.swift
+//  Airbnb
+//
+//  Created by Uday on 16/09/25.
+//
+
+import SwiftUI
+
+struct DestinationSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationSearchView()
+}
